fa6d91e888bd6f85cc5f6e2136c7b537381fef09
pandaUbuntu <fokk190e@gmail.com>
2016-05-17 23:51:02 +0300

    fix bugs

--- home_work_2/home_work_part_one.rb
+++ home_work_2/home_work_part_one.rb
@@ -15,7 +15,7 @@ class Task_two
 
   def initialize()
     @hash = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
-	puts "Start array: #{@arr}"
+	puts "Start array: #{@hash}"
   end
   
   def decision
