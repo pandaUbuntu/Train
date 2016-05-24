2c6b92690b1152caf1d44e2c3f63ed9a1b68f273
pandaUbuntu <fokk190e@gmail.com>
2016-05-25 00:00:18 +0300

    fix test

--- home_work_2/home_work_part_one.rb
+++ home_work_2/home_work_part_one.rb
@@ -13,24 +13,27 @@ end
 
 class Task_two
 
-  def initialize()
+  def initialize
     @hash = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
 	puts "Start array: #{@hash}"
   end
   
   def decision
-    @hash.keys.select{ |k| k =~ /^yes/i }.size
+    puts @hash.keys.select{ |k| k =~ /^yes/i }.size
   end
 
 end
 
-
-
-
-
-
-
-
-
-
+class TaskTest < Minitest::Test
+  def test_first_task
+    task_one = Task_one.new(3)
+	task_one.stub(:rand, 0) do
+	assert_equal(task_one.decision)
+  end
+  
+  def test_second_task
+    task_two = Task_two.new({'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014})
+	assert_equal(5, task_two.decision)
+  end
+end
 
